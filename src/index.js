cd878abf0a65c856fa443dd5b9f43f6fa76f9c9c
Jayendra <jayendra@Jayendras-MacBook-Pro.local>
2018-09-07 11:24:01 +0530

    [Validation for empty container name or media source]

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import { loadReactMediaPlayer } from "./App";
 
 (function (w) {
   w.initMediaPlayer = function (options) {
+    if (!options.container) {
+      alert ("You have not provided container to mount media player.");
+      return;
+    }
+    if (!options.source) {
+      alert ("You have not provided a source of media.");
+      return;
+    }
     loadReactMediaPlayer ({
       container: options.container,
       source: options.source,
-      isAudio: options.isAudio,
-      poster: options.poster,
-      autoPlay: options.autoPlay,
-      mute: options.mute
+      isAudio: options.isAudio || false,
+      poster: options.poster || "",
+      autoPlay: options.autoPlay || false,
+      mute: options.mute || false
     });
   }
 }) (window);
